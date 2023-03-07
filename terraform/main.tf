d269b4b8b8cf5e805affac1ee66e87f734e7f28f
Quang <opsdev48@gmail.com>
2023-03-07 12:09:31 +0700

    update info

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "tinhtq+test@agileops.vn"
+    AccountName               = "quang-tinh"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "tinhtq+test@agileops.vn"
+    SSOUserFirstName          = "Test"
+    SSOUserLastName           = "User"
   }
 
   account_tags = {
